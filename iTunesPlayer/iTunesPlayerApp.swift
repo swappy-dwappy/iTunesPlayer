e09ee2efe809db01fcd9049df5f952f30245b351
Sonkar, Swapnil <Swapnil.Swapnil@transformco.com>
2024-05-20 01:49:34 +0530

    Initial Commit

---
+++ iTunesPlayer/iTunesPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesPlayerApp.swift
+//  iTunesPlayer
+//
+//  Created by Sonkar, Swapnil on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iTunesPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
